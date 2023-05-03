7990358f0960a4ca5e4250b1d745dd7a55a47684
akhons <akhil.trv19ec008@gecbh.ac.in>
2023-05-03 19:36:54 +0530

    crawl page recursive working

--- crawl.js
+++ crawl.js
@@ -1,25 +1,47 @@
 const {JSDOM} = require('jsdom')
+// crawlPage is a recursive function...the for loop give it the nexturls to crawl again
+async function crawlPage(baseURL,currentURL,pages){
+    
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
 
-async function crawlPage(currentURL){
+    if(baseURLObj.hostname !== currentURLObj.hostname){
+       return pages
+    }
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    if(pages[normalizedCurrentURL] > 0){
+       
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+    //  crawling a new page
+    pages[normalizedCurrentURL]=1
     console.log(`actively crawling: ${currentURL}`)
-
-    try{const resp = await fetch(currentURL)
+    try{
+        const resp = await fetch(currentURL)
 
         if(resp.status>399){
             console.log(`error in fetch with status code: ${resp.status} on page:${currentURL}`)
-            return
+            return pages
         }
         const contentType = resp.headers.get("content-type")
-        if(contentType!=="text/html"){// here we can us if(!contentType.include("text/html")){}
+        if(!contentType.includes("text/html")){// here we can us if(contentType!=="text/html"){}
             console.log(`non html reponse,content type: ${contentType}, on page:${currentURL}`)
-            return
+            return pages
         }
-    console.log(await resp.text())}
-    catch(err){
+    const htmlBody = await resp.text()
+    const nextURLS = getURLsFromHTML(htmlBody,baseURL)
+
+    for( const nextURL of nextURLS){
+        //updating the pages object for the newurl by calling the crawlPage function
+        pages = await crawlPage(baseURL,nextURL,pages)
+    }
+    }catch(err){
         console.log(`error in fetch: ${err.message}, on page:${currentURL}`)
     }
+    return pages
 }
-
+//extracting all the links from the html
 function getURLsFromHTML(htmlBody,baseURL) {
     const urls =[]
     const dom = new JSDOM(htmlBody)
